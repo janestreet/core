a9f8708769ad39b8f15506c9fc06e2ff247228b3
Jeremie Dimino <jdimino@janestreet.com>
2013-03-20 09:11:07 +0000

    moved the -lrt flag from myocamlbuild.ml to _oasis

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -11,13 +11,6 @@ let dispatch = function
     flag ["mlh"; "ocaml"; "ocamldep"] (S[A"-ppopt"; A"-Ilib/"]);
     flag ["mlh"; "ocaml"; "compile"]  (S[A"-ppopt"; A"-Ilib/"]);
     flag ["mlh"; "ocaml"; "doc"]      (S[A"-ppopt"; A"-Ilib/"]);
-
-    let env = BaseEnvLight.load () in
-    let rt_possible = BaseEnvLight.var_get "rt_possible" env = "true" in
-    if rt_possible then begin
-      flag ["ocamlmklib"; "c"]                      (S[A"-lrt"]);
-      flag ["use_libcore_stubs"; "link"] (S[A"-cclib"; A"-lrt"])
-    end
   | _ ->
     ()
 
