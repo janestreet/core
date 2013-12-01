786fc0b773691ad90e9527849a303cf1828550c0
Anil Madhavapeddy <anil@recoil.org>
2013-12-01 16:17:57 +0000

    stat64 is an alias for stat on OpenBSD and FreeBSD.

--- lib/unix_stubs.c
+++ lib/unix_stubs.c
@@ -52,6 +52,10 @@
 #include <wordexp.h>
 #endif
 
+#if defined(__FreeBSD__) || defined(__OpenBSD__)
+#define stat64 stat
+#endif
+
 CAMLprim value unix_error_stub(value v_errcode, value v_cmdname, value cmd_arg)
 {
   unix_error(Int_val(v_errcode), String_val(v_cmdname), cmd_arg);
