d91ccbd9de99f1bda8563e7da840096161dd0347
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-01-14 20:28:27 -0800

    MSVC supported unused attribute
    Signed-off-by: Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>

--- core/src/gc_stubs.c
+++ core/src/gc_stubs.c
@@ -4,6 +4,15 @@
 #include <caml/memory.h>
 #include <caml/memprof.h>
 
+#if defined(_MSC_VER) && _MSC_VER >= 1500
+# define __unused(x) __pragma( warning (push) ) \
+    __pragma( warning (disable:4189 ) ) \
+    x \
+    __pragma( warning (pop))
+#else
+# define __unused(x) x __attribute__((unused))
+#endif
+
 static intnat minor_words(void) {
   return (intnat)(caml_stat_minor_words +
                   (double)(caml_young_end - caml_young_ptr));
@@ -13,7 +22,7 @@ static intnat promoted_words(void) {
   return ((intnat)caml_stat_promoted_words);
 }
 
-CAMLprim value core_gc_minor_words(value unit __attribute__((unused))) {
+CAMLprim value core_gc_minor_words(__unused(value unit)) {
   return Val_long(minor_words());
 }
 
@@ -21,49 +30,47 @@ static intnat major_words(void) {
   return (intnat)(caml_stat_major_words + (double)caml_allocated_words);
 }
 
-CAMLprim value core_gc_major_words(value unit __attribute__((unused))) {
+CAMLprim value core_gc_major_words(__unused(value unit)) {
   return Val_long(major_words());
 }
 
-CAMLprim value core_gc_promoted_words(value unit __attribute__((unused))) {
+CAMLprim value core_gc_promoted_words(__unused(value unit)) {
   return Val_long(promoted_words());
 }
 
-CAMLprim value core_gc_minor_collections(value unit __attribute__((unused))) {
+CAMLprim value core_gc_minor_collections(__unused(value unit)) {
   return Val_long(caml_stat_minor_collections);
 }
 
-CAMLprim value core_gc_major_collections(value unit __attribute__((unused))) {
+CAMLprim value core_gc_major_collections(__unused(value unit)) {
   return Val_long(caml_stat_major_collections);
 }
 
-CAMLprim value core_gc_heap_words(value unit __attribute__((unused))) {
+CAMLprim value core_gc_heap_words(__unused(value unit)) {
   return Val_long(caml_stat_heap_wsz);
 }
 
-CAMLprim value core_gc_heap_chunks(value unit __attribute__((unused))) {
+CAMLprim value core_gc_heap_chunks(__unused(value unit)) {
   return Val_long(caml_stat_heap_chunks);
 }
 
-CAMLprim value core_gc_compactions(value unit __attribute__((unused))) {
+CAMLprim value core_gc_compactions(__unused(value unit)) {
   return Val_long(caml_stat_compactions);
 }
 
-CAMLprim value core_gc_top_heap_words(value unit __attribute__((unused))) {
+CAMLprim value core_gc_top_heap_words(__unused(value unit)) {
   return Val_long(caml_stat_top_heap_wsz);
 }
 
-CAMLprim value core_gc_major_plus_minor_words(value unit
-                                              __attribute__((unused))) {
+CAMLprim value core_gc_major_plus_minor_words(__unused(value unit)) {
   return Val_long(minor_words() + major_words());
 }
 
-CAMLprim value core_gc_allocated_words(value unit __attribute__((unused))) {
+CAMLprim value core_gc_allocated_words(__unused(value unit)) {
   return Val_long(minor_words() + major_words() - promoted_words());
 }
 
-CAMLprim value core_gc_run_memprof_callbacks(value unit
-                                             __attribute__((unused))) {
+CAMLprim value core_gc_run_memprof_callbacks(__unused(value unit)) {
   value exn = caml_memprof_handle_postponed_exn();
   caml_raise_if_exception(exn);
   return Val_unit;
